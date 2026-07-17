177de43319c7b3c72d6922e25a8609253ac30c9f
zoloseconds <theoneanisstorio@gmail.com>
2026-07-17 08:23:05 +0800

    Update story.js

--- src/data/story.js
+++ src/data/story.js
@@ -7,22 +7,22 @@ import img5 from "../assets/images/img5.jpg";
 export const story = [
   {
     image: img1,
-    text: "Hello My baby, Just wanted to tell you somethingggg...",
+    text: "Hello my xiao baby, Just wanted to tell you something that is in my heart...",
   },
   {
     image: img2,
-    text: "I love you so much, you bring joy to my life.",
+    text: "I love you so much, you bring joy to my life, you uplift my happiness.",
   },
   {
     image: img3,
-    text: "Our first movie trip together 🥺",
+    text: "Our first movie trip together we had so much fun watching minions, you remember that?",
   },
   {
     image: img4,
-    text: "Last day we met at night before you left :( 🥺",
+    text: "Last day we met at night before you left, I wanted to stay in your arms forever in that moment :( ",
   },
   {
     image: img5,
-    text: "Yeah, sometimes I do stupid things and ask stupid questions am a dolbayob after all, and I am sorry for that. But just know that I love you so fucking  for life, You are my everything❤️",
+    text: "Yeah, sometimes I do stupid things and ask stupid questions am a dolbayob after all, and I am sorry for that. But just know I never want to hurt you or something like that and you never hurt me, also know that I love you so fucking much and will love you for life, You are my everything❤️",
   },
 ];
